9bc290ab76874b2fdcec1510a44093953fb5560c
jashanpratapsingh <jashanpratap123@gmail.com>
2025-01-05 23:55:22 -0500

    tested out the get requests from the browser in this case

--- app/api/actions/route.ts
+++ app/api/actions/route.ts
@@ -1,8 +1,17 @@
-import { ActionGetResponse } from "@solana/actions"
+import { ACTIONS_CORS_HEADERS, ActionGetResponse } from "@solana/actions"
 
 export const GET = (req: Request) => {
 
-    const payload : ActionGetResponse = {}
+    const payload : ActionGetResponse = {
+        icon: new URL("/solana_devs.jpg", new URL(req.url).origin).toString(),
+        label: "Send Memo",
+        description: "This is a super simple action",
+        title: "Memo Demo"
+    }
 
-    return Response.json(payload)
+    return Response.json(payload, {headers: ACTIONS_CORS_HEADERS});  
 }
+
+
+// This line gives you the maximum flexibility with the browsers
+export const OPTIONS = GET;
